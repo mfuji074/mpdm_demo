20ff4a513c41b5781a1debf149c268dba55298da
mfuji <fujiwara.masahiro1107@gmail.com>
2021-01-04 16:06:53 +0900

    fix main loop

--- src/main.py
+++ src/main.py
@@ -6,15 +6,15 @@ from car import Car
 from mpdm import MPDM
 
 # simulation period
-tf = 50
+tf = 5000
 dt = 0.1
 tspan = np.arange(dt, tf, dt)
 
 
 # car
-car0 = Car(0, 5.0,  1.0, 0.0, [1.0, 1.2]) # lane, pos, vel, acc, vel_max(各レーンでの最高速度)
-car1 = Car(0, 10.0, 1.0, 0.0, [0.8, 1.0])
-car2 = Car(1, 0.0,  1.2, 0.0, [0.9, 1.1])
+car0 = Car(0, 20.0,  0.8, 0.0, [1.0, 1.2]) # lane, pos, vel, acc, vel_max(各レーンでの最高速度)
+car1 = Car(0, 100.0, 0.7, 0.0, [0.8, 1.0])
+car2 = Car(1, 0.0,  1.0, 0.0, [0.9, 1.1])
 
 cars = [car0, car1, car2]
 
@@ -30,10 +30,14 @@ for i in range(len(tspan)):
 #    if (i % interval_mpdm) == 0:
 #            car[0].policy = mpdm_car0.optimize(cars)
 
-    for i, car in enumerate(cars):
+    for car in cars:
         # measurement
         car.measure(cars)
 
+        # policy
+        car.exec_policy(dt)
+
+    for car in cars:
         # update state
         car.update(dt)
 
@@ -43,17 +47,23 @@ for i in range(len(tspan)):
 
 # plot
 plt.figure()
-plt.subplot(3,1,1)
+plt.subplot(4,1,1)
 for i,car in enumerate(cars):
     plt.plot(tspan, car.pos_list)
 
-plt.subplot(3,1,2)
+plt.subplot(4,1,2)
 for i,car in enumerate(cars):
     plt.plot(tspan, car.vel_list)
 
-plt.subplot(3,1,3)
+plt.subplot(4,1,3)
+for i,car in enumerate(cars):
+    plt.plot(tspan, car.acc_list)
+
+plt.subplot(4,1,4)
 for i,car in enumerate(cars):
     plt.plot(tspan, car.lane_list)
 
+plt.figure()
+plt.plot(tspan, cars[1].pos_list - cars[0].pos_list)
 
 plt.show()
